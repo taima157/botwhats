10bbbaf1d12d2e0eeb22435bbc747d413f9aa236
Gustavo Taima <gustavotaima@MacBook-Pro-de-Gustavo.local>
2022-12-19 18:57:54 -0300

    Teste SSH

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import time
 
+# teste git
+
 contacts = []
 messages = []
 files = []
